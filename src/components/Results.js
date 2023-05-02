6d4bbe0fa91dab3b1e8df1ef0ae4408d6ef078ef
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-05-02 14:05:15 -0500

    Destructure Results.js props

---
+++ src/components/Results.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Results = props => {
+	/* Facilitate using the props */
+	const { firstName, lastName, email, password, confirmPassword } = props.data;
+
+	return (
+		<div>
+			
+		</div>
+	)
+}

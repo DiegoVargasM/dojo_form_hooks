5e90bd37f6561549f8517370ac4e06942037bc77
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-05-02 13:47:41 -0500

    Create onchange function in Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,29 @@
 import React from "react";
 
 const Form = props => {
+	/* Deestructuring our props */
+	const { inputs, setInputs } = props;
+
+	const onChange = e => {
+		setInputs({
+			/* Grab all */
+			...inputs,
+			/* Only change one */
+			[e.target.name]: e.target.value
+		})
+	}
 	return (
 		<form>
 			<label htmlFor="firstName" >First Name:</label>
-			<input type="text" name="firstName" ></input>
+			<input onChange={onChange} type="text" name="firstName" ></input>
 			<label htmlFor="lastName" >Last Name:</label>
-			<input type="text" name="lastName" ></input>
+			<input onChange={onChange} type="text" name="lastName" ></input>
 			<label htmlFor="email" >Email:</label>
-			<input type="text" name="email" ></input>
+			<input onChange={onChange} type="text" name="email" ></input>
 			<label htmlFor="password" >Password:</label>
-			<input type="text" name="password" ></input>
+			<input onChange={onChange} type="text" name="password" ></input>
 			<label htmlFor="confirmPassword" >Confirm Password:</label>
-			<input type="text" name="confirmPassword" ></input>
+			<input onChange={onChange} type="text" name="confirmPassword" ></input>
 		</form>	
 	)
 }
